c546b0688185e0b1a065bde10f5d278efcde6ffc
marxxcodes <marxx171@gmail.com>
2021-05-18 15:19:51 -0300

    Desafio 3 Vnw

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// 1
+for(let x = 1; x <= 10; x +=1){
+    console.log(x)
+}
+// 2
+let x = 11
+while(x > 1){
+    x--
+    console.log(x)
+}
+// 3
+for(let y = 1; y <= 100; y+=2){
+    console.log(y)
+}
+// 4
+let y = 0
+while(y < 100){
+    y+=2
+    console.log(y)
+}
